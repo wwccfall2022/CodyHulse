4e12821c6c029b393a38ce22cccda1d72a636281
CodyHulse <113930918+CodyHulse@users.noreply.github.com>
2022-11-30 18:49:13 -0800

    Forgot to remove semicolon...

--- week7/answers.sql
+++ week7/answers.sql
@@ -115,5 +115,5 @@ CREATE OR REPLACE VIEW character_items AS
 			INNER JOIN equipped e
 				ON c.character_id = e.character_id
 			INNER JOIN items it
-				ON e.item_id = it.item_id;
+				ON e.item_id = it.item_id
 	ORDER BY item_name ASC;
